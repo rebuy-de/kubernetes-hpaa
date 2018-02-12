da43fd1df97f5ac4555603daaad73f001f3ac6ca
Sven Walter <s.walter@rebuy.com>
2018-02-12 22:24:46 +0100

    add handler

--- cmd/handler.go
+++ cmd/handler.go
@@ -1,7 +1,71 @@
 package cmd
 
-import "k8s.io/api/autoscaling/v1"
+import (
+	"fmt"
+	"strconv"
+	"time"
 
-func Handle(hpa *v1.HorizontalPodAutoscaler) (*v1.HorizontalPodAutoscaler, error) {
+	"k8s.io/api/autoscaling/v1"
+	"k8s.io/apimachinery/pkg/util/clock"
+)
+
+type Handler struct {
+	clock clock.Clock
+
+	DownscaleCooldown time.Duration
+}
+
+func NewHandler(cd time.Duration) *Handler {
+	return &Handler{
+		clock: new(clock.RealClock),
+
+		DownscaleCooldown: cd,
+	}
+}
+
+func (h *Handler) Run(hpa *v1.HorizontalPodAutoscaler) (*v1.HorizontalPodAutoscaler, error) {
+	now := h.clock.Now()
+	last := hpa.Status.LastScaleTime
+	deadline := last.Add(h.DownscaleCooldown)
+
+	currReplicas := hpa.Status.CurrentReplicas
+	lowerLimit, err := getAnnotationInt(hpa, "lower-replica-limit")
+	if err != nil {
+		return nil, err
+	}
+
+	newMinReplicas := currReplicas
+
+	if now.After(deadline) {
+		newMinReplicas--
+	}
+
+	newMinReplicas = max(newMinReplicas, lowerLimit)
+	hpa.Spec.MinReplicas = &newMinReplicas
+
 	return hpa, nil
+}
+
+func max(a, b int32) int32 {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func getAnnotationInt(hpa *v1.HorizontalPodAutoscaler, name string) (int32, error) {
+	tpl := "rebuy.com/kubernetes-hpaa.%s"
+	key := fmt.Sprintf(tpl, name)
+
+	s, ok := hpa.ObjectMeta.Annotations[key]
+	if !ok {
+		return 0, fmt.Errorf("no annotation with key %s found", key)
+	}
+
+	i, err := strconv.Atoi(s)
+	if err != nil {
+		return 0, err
+	}
+
+	return int32(i), nil
 }
--- cmd/handler_test.go
+++ cmd/handler_test.go
@@ -1,40 +1,137 @@
-package cmd_test
+package cmd
 
 import (
-	"encoding/json"
-	"io/ioutil"
-	"path"
+	"fmt"
 	"testing"
+	"time"
+
+	v1meta "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	"k8s.io/api/autoscaling/v1"
-
-	"github.com/rebuy-de/kubernetes-hpaa/cmd"
-	"github.com/rebuy-de/rebuy-golang-sdk/testutil"
+	"k8s.io/apimachinery/pkg/util/clock"
 )
 
-func testReadHPA(tb testing.TB, name string) *v1.HorizontalPodAutoscaler {
-	tb.Helper()
-
-	raw, err := ioutil.ReadFile(path.Join("test-fixtures", name+".json"))
-	if err != nil {
-		tb.Fatal(err)
-	}
-
+func testCreateHPA() *v1.HorizontalPodAutoscaler {
 	hpa := new(v1.HorizontalPodAutoscaler)
-
-	err = json.Unmarshal(raw, hpa)
-	if err != nil {
-		tb.Fatal(err)
-	}
+	hpa.Spec.MaxReplicas = 40
+	min := int32(6)
+	hpa.Spec.MinReplicas = &min
+	t := v1meta.Date(
+		2018, 02, 12,
+		20, 56, 0, 0,
+		time.FixedZone("UTC", 0))
+	hpa.Status.LastScaleTime = &t
 
 	return hpa
 }
 
-func TestHandle(t *testing.T) {
-	hpa := testReadHPA(t, "handler-1")
-	hpa, err := cmd.Handle(hpa)
-	if err != nil {
-		t.Fatal(err)
+func TestHandleValidCases(t *testing.T) {
+	cases := []struct {
+		name            string
+		offset          time.Duration
+		lowerLimit      int32
+		currentReplicas int32
+		expect          int32
+	}{
+		{
+			name:            "WithinColldown",
+			offset:          3 * time.Minute,
+			lowerLimit:      6,
+			currentReplicas: 11,
+			expect:          11,
+		},
+		{
+			name:            "AfterCooldown",
+			offset:          10 * time.Minute,
+			lowerLimit:      6,
+			currentReplicas: 11,
+			expect:          10,
+		},
+		{
+			name:            "NoActionLowerLimit",
+			offset:          10 * time.Minute,
+			lowerLimit:      11,
+			currentReplicas: 11,
+			expect:          11,
+		},
+		{
+			name:            "EnforceLowerLimit",
+			offset:          10 * time.Minute,
+			lowerLimit:      12,
+			currentReplicas: 11,
+			expect:          12,
+		},
+		{
+			name:            "ScaleUpWithinCooldown",
+			offset:          2 * time.Minute,
+			lowerLimit:      12,
+			currentReplicas: 20,
+			expect:          20,
+		},
+		{
+			name:            "ScaleUpAfterCooldown",
+			offset:          10 * time.Minute,
+			lowerLimit:      12,
+			currentReplicas: 20,
+			expect:          19,
+		},
 	}
-	testutil.AssertGoldenJSON(t, "test-fixtures/handler-1-golden.json", hpa)
+
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			hpa := testCreateHPA()
+			hpa.Status.CurrentReplicas = tc.currentReplicas
+			hpa.ObjectMeta.Annotations = map[string]string{
+				"rebuy.com/kubernetes-hpaa.lower-replica-limit": fmt.Sprint(tc.lowerLimit),
+			}
+
+			handler := NewHandler(5 * time.Minute)
+			handler.clock = clock.NewFakeClock(hpa.Status.LastScaleTime.Add(tc.offset))
+
+			hpa, err := handler.Run(hpa)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			have := *hpa.Spec.MinReplicas
+			want := tc.expect
+			if want != have {
+				t.Fatalf("Wrong `spec.minReplicas`. Want: %d. Have: %d", want, have)
+			}
+		})
+	}
+}
+
+func TestHandleMissingAnnotation(t *testing.T) {
+	hpa := testCreateHPA()
+	handler := NewHandler(5 * time.Minute)
+	_, err := handler.Run(hpa)
+	if err == nil {
+		t.Fatal("Expected an error")
+	}
+
+	want := "no annotation with key rebuy.com/kubernetes-hpaa.lower-replica-limit found"
+	have := err.Error()
+	if want != have {
+		t.Fatalf("Wrong error. Want `%s`. Have: `%s`.", want, have)
+	}
+}
+
+func TestHandleInvalidAnnotation(t *testing.T) {
+	hpa := testCreateHPA()
+	hpa.ObjectMeta.Annotations = map[string]string{
+		"rebuy.com/kubernetes-hpaa.lower-replica-limit": "foo",
+	}
+
+	handler := NewHandler(5 * time.Minute)
+	_, err := handler.Run(hpa)
+	if err == nil {
+		t.Fatal("Expected an error")
+	}
+
+	want := `strconv.Atoi: parsing "foo": invalid syntax`
+	have := err.Error()
+	if want != have {
+		t.Fatalf("Wrong error. Want `%s`. Have: `%s`.", want, have)
+	}
 }
