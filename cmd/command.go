59f09f1412497c9ad72078c9721cb56be87e4547
Sven Walter <s.walter@rebuy.com>
2018-02-14 10:50:36 +0100

    add Graylog

--- cmd/command.go
+++ cmd/command.go
@@ -3,6 +3,8 @@ package cmd
 import (
 	"time"
 
+	graylog "gopkg.in/gemnasium/logrus-graylog-hook.v2"
+
 	"github.com/rebuy-de/rebuy-go-sdk/cmdutil"
 
 	log "github.com/sirupsen/logrus"
@@ -24,6 +26,15 @@ func NewRootCommand() *cobra.Command {
 		Run:   app.Run,
 		PersistentPreRun: func(cmd *cobra.Command, args []string) {
 			log.SetLevel(log.DebugLevel)
+			if app.Params.GraylogAddress != "" {
+				labels := map[string]interface{}{
+					"facility":   "kubernetes-hpaa",
+					"version":    BuildVersion,
+					"commit-sha": BuildHash,
+				}
+				hook := graylog.NewGraylogHook(app.Params.GraylogAddress, labels)
+				log.AddHook(hook)
+			}
 		},
 	}
 
@@ -36,8 +47,9 @@ func NewRootCommand() *cobra.Command {
 
 type App struct {
 	Params struct {
-		Kubeconfig string
-		Namespace  string
+		Kubeconfig     string
+		Namespace      string
+		GraylogAddress string
 	}
 }
 
@@ -48,6 +60,9 @@ func (app *App) Bind(cmd *cobra.Command) {
 	cmd.PersistentFlags().StringVarP(
 		&app.Params.Namespace, "namespace", "n", "",
 		"Namespace to watch. Keep empty for all namespaces.")
+	cmd.PersistentFlags().StringVar(
+		&app.Params.GraylogAddress, "graylog-address", "",
+		`Address to Graylog for logging (format: "ip:port").`)
 }
 
 func (app *App) Kubernetes() kubernetes.Interface {
@@ -81,6 +96,13 @@ func (app *App) Kubernetes() kubernetes.Interface {
 }
 
 func (app *App) Run(cmd *cobra.Command, args []string) {
+	log.WithFields(log.Fields{
+		"Version": BuildVersion,
+		"Date":    BuildDate,
+		"Commit":  BuildHash,
+	}).Info("kubernetes-hpaa started")
+	defer log.Info("stopping kubernetes-hpaa")
+
 	client := app.Kubernetes()
 	stopCh := make(<-chan struct{})
 
